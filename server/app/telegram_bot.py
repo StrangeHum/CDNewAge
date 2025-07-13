649323b28857ed592f77b05f6e7a3b567ecb857e
StrangeHum <vadim.k.2004@yandex.ru>
2025-07-13 12:23:22 +0300

    bot

--- server/app/telegram_bot.py
+++ server/app/telegram_bot.py
@@ -1,18 +1,44 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import asyncio
+import logging
+import sys
+from os import getenv
 
-# import os
-# from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from faststream.rabbit import RabbitBroker
 
 TOKEN = "6251377163:AAGHocnHBFunewuf0S_D3aOexRfkhxCIsSo"
+# TOKEN = getenv("BOT_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот готов принимать сообщения!")
 
-def create_bot():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    print('Telegramm bot has been runned')
-    return app
+dp = Dispatcher()
 
-# create_bot().run_polling()
+bot = Bot(token=TOKEN)
+broker = RabbitBroker()
+
+
+@broker.subscriber("orders")
+async def handle_message(data: str):
+    await bot.send_message(chat_id=1753986163, text=data)
+    return data
+
+
+# @dp.message(CommandStart())
+# async def command_start_handler(message: Message) -> None:
+#     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+async def main() -> None:
+    async with broker:
+        await broker.start()
+        logging.info("Брокер стартовал")
+        await dp.start_polling(bot)
+    logging.info("Конец")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
--- server/init.py
+++ server/init.py
@@ -1,7 +1,19 @@
+# docker run --rm -p 15672:15672  rabbitmq:3.10.7-management
+# docker run -d --name rabbitmq --hostname rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3.10.7-management
 from fastapi import FastAPI
-from app import broker
-from app.telegram_bot import create_bot
+from faststream.rabbit.fastapi import RabbitRouter
 
- 
+app = FastAPI()
+router = RabbitRouter()
 
 
+@router.post("/order")
+async def make_order(name: str):
+    await router.broker.publish(
+        f"Новый заказ: {name}",
+        queue="orders",
+    )
+    return {"data": "OK"}
+
+
+app.include_router(router)
