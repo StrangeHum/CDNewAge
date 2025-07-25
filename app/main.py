122f4f801f9fa0199850918e3397dfc15adda5d8
StrangeHum <vadim.k.2004@yandex.ru>
2025-07-25 14:34:43 +0300

    немного изменена структура, ничего не работает

--- app/main.py
+++ app/main.py
@@ -1,14 +1,30 @@
 # docker run --rm -p 15672:15672  rabbitmq:3.10.7-management
 # docker run -d --name rabbitmq --hostname rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3.10.7-management
+# from fastapi import FastAPI
+# import uvicorn
+
+# app = FastAPI()
+
+
+# @app.get("/users")
+# async def make_order():
+#     return [{"id": 1, "name": "src"}]
+
+
+# @app.get("/foo")
+# async def make_foo():
+#     return [{"id": 1, "name": "foo"}]
+
+
 from fastapi import FastAPI
 from faststream.rabbit.fastapi import RabbitRouter
 
 app = FastAPI()
-router = RabbitRouter()
+router = RabbitRouter("amqp://guest:guest@localhost:5672/")
 
 
 @router.get("/")
-async def make_order():
+async def foo():
     return {"data": "OK"}
 
 
